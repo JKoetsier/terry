dbfa87e87e6172d9c07fc75350fdad3e99773158
Jaap Koetsier <jaap@chariot-it.nl>
2018-04-10 10:20:05 +0200

    Initial commit

---
+++ src/main/java/nl/jkoetsier/uva/dbbench/DbbenchApplication.java
@@ -0,0 +1,18 @@
+package nl.jkoetsier.uva.dbbench;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbbenchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbbenchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Here we are");
+	}
+}
---
+++ src/test/java/nl/jkoetsier/uva/dbbench/DbbenchApplicationTests.java
@@ -0,0 +1,16 @@
+package nl.jkoetsier.uva.dbbench;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DbbenchApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
